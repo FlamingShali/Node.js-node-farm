515866c452581969375213fe007595156eb7d460
Michał <michal.klepner1@o2.pl>
2024-08-10 06:22:43 +0200

    added nodemon & slugify

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 This project is part of the course "Node.js, Express, MongoDB & More: The Complete Bootcamp"
 
 There can be some trash/commented code or unimportant pieces of code since it is a part of course. I will clear everything for the last commit in the end of the project.
+
+Description:
+
+In order to start just running project just type "node script.js" and go to the website using your local host.
+ReferenceCode is just an old code from the begining of this project and i use it only to observe progress. 
--- script.js
+++ script.js
@@ -3,35 +3,7 @@ const http = require("http");
 const url = require("url");
 
 const replaceTemplate = require('./modules/replaceTemplate')
-
-//Blocking synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-
-// const textOut = `This is what we know about this avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File written!");
-
-//Non-blocking, asynchronous way
-// fs.readFile("./txt/start.txt", 'utf-8', (err, data1) => {
-//     if(err) return console.log('Error! 🛑')
-//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//         console.log(data2);
-//         fs.readFile("./txt/append.txt", 'utf-8', (err, data3) => {
-//             console.log(data3);
-
-//             fs.writeFile('./txt/final.txt', `${data2}/n${data3}`, 'utf-8', err => {
-//                 console.log('Your file has been written 😁')
-//             })
-//           });
-//       });
-// });
-
-// console.log('Will read file!')
-
 //Server
-
-
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
